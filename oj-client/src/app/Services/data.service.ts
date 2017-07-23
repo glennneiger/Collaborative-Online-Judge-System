5f66c96657fcfef576022c03d419260d425d43b7
Di Wang <jackiewang5566@hotmail.com>
2017-07-23 01:58:44 -0400

    update client side service to call server side endpoint

--- oj-client/src/app/Services/data.service.ts
+++ oj-client/src/app/Services/data.service.ts
@@ -1,24 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
+
 import { Problem } from '../data-structure/problem';
 import { PROBLEMS } from '../mock-problem';
 
 @Injectable()
 export class DataService {
-  problems: Problem[] = PROBLEMS;
+  private _problemSource = new BehaviorSubject<Problem[]>([]);
 
-  constructor() { }
+  constructor(private http: Http) { }
 
-  getProblems(): Problem[] {
-    return this.problems;
+  getProblems(): Observable<Problem[]> {
+    // return this.problems;
+    this.http.get('api/v1/problems')
+            .toPromise()
+            .then((res: Response) => {
+              this._problemSource.next(res.json());
+            })
+            .catch(this.handleError);
+    return this._problemSource.asObservable();            
   }
 
-  getProblem(id: number): Problem {
-    return this.problems.find((problem) => problem.id == id);
+  getProblem(id: number) {
+    this.http.get(`api/v1/problems/${id}`)
+          .toPromise()
+          .then((res: Response) => {
+            return res.json();
+          })
+          .catch(this.handleError);
   }
 
   addProblem(problem: Problem) {
-    problem.id = this.problems.length + 1;
-    this.problems.push(problem);
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({ headers: headers});
+    return this.http.post('api/v1/problems', problem, options)
+                  .toPromise()
+                  .then((res: Response) => {
+                    this.getProblems();
+                    return res.json();
+                  })
+                  .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.log('An error occured', error);
+    return Promise.reject(error);
   }
 
 }
--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './Components/problem-list/problem-list.component';
@@ -25,6 +26,7 @@ import { NewProblemComponent } from './Components/new-problem/new-problem.compon
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   providers: [
